4d40bc0e227ddfcff787c80a8f869ecfaaf7dbf4
OGuerreroArias <89095446+OGuerreroArias@users.noreply.github.com>
2022-11-10 16:05:25 -0500

    add bc tour

---
+++ src/main/java/com/api/lifetravel/tours/resource/TourImageResource.java
@@ -0,0 +1,14 @@
+package com.api.lifetravel.tours.resource;
+
+import lombok.*;
+
+@Getter
+@Setter
+@With
+@NoArgsConstructor
+@AllArgsConstructor
+public class TourImageResource {
+     private Long id;
+     private String path;
+
+}
